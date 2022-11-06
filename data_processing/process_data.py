278f548da8f7ac24ea41b22fdaf4ef93e883fbfa
benpm <schwalegos@gmail.com>
2022-11-06 01:05:16 -0600

    mostly done with data processing

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 movies = pd.read_csv("raw_data/movies.csv")
 scores = pd.read_csv("raw_data/rotten_tomatoes_movies.csv")
@@ -12,24 +13,42 @@ scores = scores[["movie_title", "tomatometer_rating", "audience_rating"]]
 scores = scores.rename(columns={"movie_title": "name"})
 movies = movies.merge(scores, on="name")
 
-# Get year_ceremony, ceremony, category, winner from oscars and merge with movies
-oscars = oscars[["film", "year_ceremony", "ceremony", "winner", "year_film"]]
-oscars = oscars.rename(columns={"film": "name"})
+# # Get year_ceremony, ceremony, category, winner from oscars and merge with movies
+# oscars = oscars[["film", "year_ceremony", "ceremony", "winner", "year_film"]]
+# oscars = oscars.rename(columns={"film": "name"})
 
-# Count each occurence of name and assign that count to a new column nominations
-nomcounts = oscars["name"].value_counts()
-movies["nominations"] = movies["name"].apply(lambda x: nomcounts[x] if x in nomcounts else 0)
-temp = oscars.drop_duplicates(subset=["name", "year_film"])
-temp = temp.set_index(["name", "year_film"])
-print(temp)
-movies["year_ceremony"] = movies["name"].apply(lambda x: temp.loc[x].year_ceremony if x in temp.index else None)
-
-# Remove rows with same name and year
-movies = movies.drop_duplicates(subset=["name", "year"])
+# # Remove rows with same name and year
+# movies = movies.drop_duplicates(subset=["name", "year"])
 
 # Remove unused columns
-# remove_fields = ["rating", "star", "writer", "director", "category"]
-# movies = movies.drop(remove_fields, axis=1)
+remove_fields = ["rating", "star", "writer", "director"]
+movies = movies.drop(remove_fields, axis=1)
+
+# Remove rows missing important info
+movies = movies.dropna(subset=["released", "budget", "gross", "score", "year", "genre", "company"])
+
+# Remove country from release date
+movies["released"] = movies["released"].apply(lambda x: re.sub(r" \(.*\)", "", x).strip())
+
+# Remove duplicates by name and year
+movies = movies.drop_duplicates(subset=["name", "year"])
+movies.set_index(["name", "year"], inplace=True)
+
+# For each movie, set the overall oscar status
+movies["oscar"] = "none"
+def set_oscar(row):
+    if row["ceremony"] in ["BEST PICTURE", "BEST MOTION PICTURE"]:
+        label = "best_picture_winner" if row["winner"] else "best_picture_nominee"
+    else:
+        label = "winner" if row["winner"] else "nominee"
+    idx = (row["film"], row["year_film"])
+    if idx in movies.index:
+        movies.loc[idx, "oscar"] = label
+oscars.apply(set_oscar, axis=1)
+
+# Count each occurence of name and assign that count to a new column nominations
+nomcounts = oscars["film"].value_counts()
+movies["nominations"] = movies.index.map(lambda x: nomcounts[x[0]] if x[0] in nomcounts else 0)
 
 # Save to csv
-movies.to_csv("movies.csv", index=False)
+movies.to_csv("../public/movies.csv", index=True)
