36131528ada7eca3b7a6dd538b32ff6bf01cf2b8
benpm <schwalegos@gmail.com>
2022-11-05 18:46:23 -0600

    init react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './scripts/dashboard.js'
+
+function App() {
+  let dashboard = new Dashboard();
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello world
+      </header>
+    </div>
+  );
+}
+
+export default App;
