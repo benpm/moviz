644d2d1e9e5207c654317e8ab0c839fab03170f6
benpm <schwalegos@gmail.com>
2022-11-06 18:59:08 -0700

    trying stuff

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,9 +1,9 @@
-import Scatterplot from './Scatterplot';
+import CScatterplot from './Scatterplot';
 
 const Dashboard = () => {
   return (
     <div id="dashboard" className="p-2 w-full h-full grid grid-cols-2 grid-rows-2 gap-2">
-      <div className="bg-slate-200 col-span-2"><Scatterplot /></div>
+      <div className="bg-slate-200 col-span-2"><CScatterplot /></div>
       <div className="bg-slate-200"></div>
       <div className="bg-slate-200"></div>
     </div>
--- components/Scatterplot.jsx
+++ components/Scatterplot.jsx
@@ -1,10 +1,23 @@
-const Scatterplot = () => {
+import rd3 from "react-d3-library";
+import * as d3 from "d3";
+import { useEffect, useState } from "react";
+const RD3Component = rd3.Component;
+
+export default function CScatterplot() {
+    const [data, setData] = useState({ d3: "" });
+    // useEffect(() => {
+    //     setData({ d3: d3.csv("movies.csv", d3.autoType)});
+    //     console.log(data);
+    // }, []);
+    d3.csv("movies.csv", d3.autoType).then(data => {
+        console.log(data);
+        setData({ d3: data });
+    });
+
     return (
         <div id="scatterplot" className="w-full h-full bg-violet-200">
             <p>Scatterplot</p>
-            <svg></svg>
+            {data ? <RD3Component data={data} /> : <p>Loading...</p>}
         </div>
     );
-}
-
-export default Scatterplot;
+};
