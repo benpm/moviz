0604edd0ce28e2414b627ef1ef8f94bde28476f2
benpm <schwalegos@gmail.com>
2022-11-09 11:00:23 -0700

    deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/benpm/moviz/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
