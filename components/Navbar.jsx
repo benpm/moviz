bf8edebf635445b81d8fa15190ee410c78d619ad
benpm <schwalegos@gmail.com>
2022-11-13 15:10:27 -0700

    fixed logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,8 @@
-import { default as logo } from '../logo.svg';
-
 export default function CNavBar() {
     //console.log(Logo);
     return (
-        <div className="bg-slate-700 flex flex-row text-slate-100">
-            <img src={logo} alt="logo" />
-            <div className="flex flex-row">
-                <div className="p-2">menu2</div>
-                <div className="p-2">menu3</div>
-            </div>
+        <div className="bg-slate-700 flex flex-row text-slate-100 h-16">
+            <img src="logo.svg" alt="logo" className="h-full p-1" />
         </div>
     );
 }
