597fd2c3efe2fd83d7317ee838de41fdac9d7c8f
Alper Şahıstan <alpersahistan@gmail.com>
2022-11-13 16:25:51 -0700

    Upload white logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
 export default function CNavBar() {
     //console.log(Logo);
     return (
-        <div className="bg-slate-700 flex flex-row text-slate-100 h-16">
-            <img src="logo.svg" alt="logo" className="h-full p-1" />
+        <div className="bg-slate-900 flex flex-row text-slate-100 h-16">
+            <img src="logo-white.svg" alt="logo" className="h-full p-1" />
         </div>
     );
 }
