265dc846abbb32e8f89347f56d9be36bc92d110a
Alper Şahıstan <alpersahistan@gmail.com>
2022-11-24 03:45:44 -0700

    Add todo instructions

--- components/Panels.jsx
+++ components/Panels.jsx
@@ -4,13 +4,20 @@ import useGlobalState from "../hooks/useGlobalState";
 
 const CLeftPanel = function({ }) {
     const [viewMode, setViewMode] = useGlobalState(s => [s.viewMode, s.setViewMode]);
-    
+    //TODO:global controls related stuff
+    //TODO:A switch to enable brushing mode instead of panning
+    //TODO:option to adjust bubble radius scale maybe ?
+    //TODO:searchbar to filter movies
 
     return (<p>Left Panel</p>);
 }
 
 const CRightPanel = function ({ }) {
     const [viewMode, setViewMode] = useGlobalState(s => [s.viewMode, s.setViewMode]);
+    //TODO:View specific stuff
+    //TODO:if economic metrics are rendered adjust for inflation switch
+    //TODO:if movie economy view a checkbox to display avg trend as a line
+    //TODO:story telling mode buttons we can higligth the some interesting points in the data
 
     return (<p>Right Panel</p>);
 }
