9a34450fc168e46e7ebdd3e115bedf357a7d793f
Alper Şahıstan <alpersahistan@gmail.com>
2022-12-01 17:46:00 -0700

    Change colormap

--- components/CompanionPlot.jsx
+++ components/CompanionPlot.jsx
@@ -198,7 +198,34 @@ function drawStackedLineChart(svg, data, bounds, margin, xAxisObj, yAxisObj, set
     stackedStudioBudgetByYear = stackedStudioBudgetByYear(studioBudgetByYear);
     //create a categorical color scale for every studio from the top 25\
     let colorScale = d3.scaleOrdinal(
-        [...d3.schemeSpectral[10], ...d3.schemePRGn[7]])
+        [
+            '#32964d',
+        '#21f0b6',
+        '#aec1b8',
+        '#ace1b7',
+        '#0a7fb2',
+        '#9ab9f9',
+        '#82269b',
+        '#fa79f5',
+        '#997c97',
+        '#20d8fd',
+        '#135ac2',
+        '#faafe3',
+        '#7d0af6',
+        '#bae342',
+        '#8b6033',
+        '#fe8f06',
+        '#a20655',
+        '#fb8c77',
+        '#d02023',
+        '#d8c3b4',
+        '#738a69',
+        '#67f059',
+        '#e0c645'
+
+            //...d3.schemeSpectral[10], ...d3.schemePRGn[7]
+        ]
+            )
         .domain(/*toggleOtherStudios ?*/['Other', allStudios] /*: allStudios*/);
 
     //get maximum budget overall filter out entry with key 'year'
